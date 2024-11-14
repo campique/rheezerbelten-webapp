aba144b15c2e64cc87b8d6d1cf762fb7c74b5e3d
campique <info@campique.com>
2024-11-14 11:04:33 +0100

    Create PancakeDobble.js

---
+++ src/games/PancakeDobble.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const PancakeDobble = () => {
+  return (
+    <div>
+      <h2>Pancake Dobble</h2>
+      <p>This is the Pancake Dobble game. Implementation coming soon!</p>
+    </div>
+  );
+};
+
+export default PancakeDobble;
