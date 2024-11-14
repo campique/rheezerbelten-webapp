a5321df534a93dc5277535bf5ab89cfe96bf7878
campique <info@campique.com>
2024-11-14 11:04:50 +0100

    Create Pictionary.js

---
+++ src/games/Pictionary.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Pictionary = () => {
+  return (
+    <div>
+      <h2>Pictionary</h2>
+      <p>This is the Pictionary game. Implementation coming soon!</p>
+    </div>
+  );
+};
+
+export default Pictionary;
