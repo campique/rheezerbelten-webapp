89d4e7363fba5bd5710f245a09feda3fa31395da
campique <info@campique.com>
2024-11-14 10:02:40 +0100

    Update Logo.js

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,1 +1,19 @@
+import React from 'react';
+import styled from 'styled-components';
 
+const LogoImage = styled.img`
+  width: 180px;
+  margin-bottom: 1rem;
+  margin-top: -2rem;
+`;
+
+const Logo = () => {
+  return (
+    <LogoImage 
+      src="https://www.vechtdalproducten.nl/wp-content/uploads/2017/05/rheezerbelten-logo.png" 
+      alt="Rheezerbelten Logo" 
+    />
+  );
+};
+
+export default Logo;
