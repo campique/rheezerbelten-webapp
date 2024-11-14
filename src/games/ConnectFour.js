20786eab6aaf8159e15a6846e7dd5ed2286c41e8
campique <info@campique.com>
2024-11-14 11:04:12 +0100

    Create ConnectFour.js

---
+++ src/games/ConnectFour.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ConnectFour = () => {
+  return (
+    <div>
+      <h2>Connect Four</h2>
+      <p>This is the Connect Four game. Implementation coming soon!</p>
+    </div>
+  );
+};
+
+export default ConnectFour;
