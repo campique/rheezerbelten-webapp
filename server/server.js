ab46ac23119af6a4dc62cc8d6a03cfc1b3278263
campique <info@campique.com>
2024-11-15 09:52:24 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -134,12 +134,12 @@ io.on('connection', (socket) => {
 });
 
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
