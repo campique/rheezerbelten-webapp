68a5dba13870859c99b82eec6675e26dfa313bac
campique <info@campique.com>
2024-11-15 21:32:10 +0100

    Update ConnectFourOptions.js

--- src/games/ConnectFourOptions.js
+++ src/games/ConnectFourOptions.js
@@ -16,7 +16,7 @@ const OptionButton = styled.div`
   color: white;
   padding: 15px 20px;
   margin: 10px;
-  border-radius: 10px;
+  border-radius: 25px;
   cursor: pointer;
   display: flex;
   align-items: center;
