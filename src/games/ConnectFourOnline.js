19b9f89e621912b625dd995b06f36841f073d6e7
campique <info@campique.com>
2024-11-15 09:46:59 +0100

    Update ConnectFourOnline.js

--- src/games/ConnectFourOnline.js
+++ src/games/ConnectFourOnline.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3001'); // Vervang dit met je server URL
+const socket = io(process.env.REACT_APP_SERVER_URL || '');
 
 const ConnectFourOnline = () => {
   const [playerName, setPlayerName] = useState('');
